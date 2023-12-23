c862dbddfbad353dbc69af98d3b2e0fc6dad4cc0
Hanss <hanss.rivera@gmail.com>
2023-12-22 18:42:36 -0700

    prog

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ from data import data
 app = Flask(__name__)
 app.register_blueprint(data)
 
-# @app.route("/")
-# def home():
-#     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
