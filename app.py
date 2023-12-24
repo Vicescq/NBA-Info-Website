6c1d3a1aec715496ac9841d67da6d9f9b1494c14
Hanss <hanss.rivera@gmail.com>
2023-12-24 02:27:12 -0700

    prog

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
-from nba_api.live.nba.endpoints import scoreboard
 from functions import *
+import requests
 
 app = Flask(__name__)
 
+
+
+
+
 @app.route("/")
 def home():
-    games = scoreboard.ScoreBoard().games.get_dict()
+    games = get_livegames()
     game_count = get_game_count(games)
     matchups = get_matchups(games)
     return render_template("index.html", game_count=game_count, matchups=matchups)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- functions.py
+++ functions.py
@@ -1,16 +1,24 @@
+import requests
+
+def get_livegames():
+    url = "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json"
+    response = requests.get(url)
+    response_json = response.json()
+    games = response_json["scoreboard"]["games"]
+    return games
+
 def get_game_count(games):
-    for index, game in enumerate(games):
-        pass
-    game_count = index + 1
-    return game_count
+    return len(games)
 
 def get_matchups(games):
+    """
+    returns pairwise strings that are the away [0] and home [1]
+    """
     matchups = []
     for game in games:
         away_team = game["awayTeam"]["teamCity"] + " " + game["awayTeam"]["teamName"]
         home_team = game["homeTeam"]["teamCity"] + " " + game["homeTeam"]["teamName"]
         curr_tuple = away_team, home_team
         matchups.append(curr_tuple)
-    
     return matchups
 
