ddb000976705bb978488c6f390a62538f521897f
Hanss <hanss.rivera@gmail.com>
2023-12-25 04:06:41 -0700

    functions

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ def home():
     games = get_livegames()
     game_count = get_game_count(games)
     matchups = get_matchups(games)
-    return render_template("index.html", game_count=game_count, matchups=matchups)
+    team_records = get_team_records(games)
+    livescores = get_livescores(games)
+    game_status = get_gamestatus(games)
+    logos = assign_logos(matchups)
+    return render_template("index.html", game_count=game_count, matchups=matchups, team_records=team_records, livescores=livescores, game_status=game_status, logos=logos)
 
 @app.route("/update")
 def update():
--- functions.py
+++ functions.py
@@ -12,15 +12,51 @@ def get_game_count(games):
 
 def get_matchups(games):
     """
-    returns pairwise strings that are the away [0] and home [1]
+    returns list of pairwise strings that are the away [0] and home [1]
     """
     matchups = []
     for game in games:
-        away_team = game["awayTeam"]["teamCity"] + " " + game["awayTeam"]["teamName"]
-        home_team = game["homeTeam"]["teamCity"] + " " + game["homeTeam"]["teamName"]
-        curr_tuple = away_team, home_team
-        matchups.append(curr_tuple)
+        away_team = game["awayTeam"]["teamTricode"]
+        home_team = game["homeTeam"]["teamTricode"] 
+        matchups.append((away_team, home_team))
     return matchups
+
+def get_team_records(games):
+    """
+    returns list of pairwise strings (eg. ("82-0", "0-82")) that are the away [0] and home [1]
+    """
+    records = []
+    for game in games:
+        away_rec = str(game["awayTeam"]["wins"]) + "-" + str(game["awayTeam"]["losses"])
+        home_rec = str(game["homeTeam"]["wins"]) + "-" + str(game["homeTeam"]["losses"])
+        records.append((away_rec, home_rec))
+    return records
+
+def get_livescores(games):
+    scores = []
+    for game in games:
+        away_score = str(game["awayTeam"]["score"])
+        home_score = str(game["homeTeam"]["score"])
+        scores.append((away_score, home_score))
+    return scores
+
+def get_gamestatus(games):
+    status = []
+    for game in games:
+        if (game["gameStatusText"] == "Final"):
+            status.append("END")
+        else:
+            status.append("TEMP") # change when more info arrives
+    return status
+        
+def assign_logos(matchups):
+    logos = []
+    for matchup in matchups:
+        away_logo = "..\\static\\images\\logos\\" + matchup[0] + ".svg"
+        home_logo = "..\\static\\images\\logos\\" + matchup[1] + ".svg"
+        logos.append((away_logo, home_logo))
+    return logos
+
 
 def gen_randnum():
     url = "https://csrng.net/csrng/csrng.php?min=1&max=100"
