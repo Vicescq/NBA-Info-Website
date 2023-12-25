fca8e177ee637dace11332b01754ba575ed917c9
Hanss <hanss.rivera@gmail.com>
2023-12-25 16:14:42 -0700

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from functions import *
+from static.functions import *
 import requests
 
 app = Flask(__name__)
--- static/2.js
+++
@@ -1,2 +0,0 @@
-// script1.js
-console.log("Hello, GitHub! This is script1.js");
