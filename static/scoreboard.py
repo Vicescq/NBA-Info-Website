7d650b62a7e30e1b7e03c679071199e3cb3384b0
Hanss <hanss.rivera@gmail.com>
2023-12-27 07:08:29 -0700

    added date conversion

--- static/scoreboard.py
+++ static/scoreboard.py
@@ -2,6 +2,9 @@ from datetime import datetime
 import requests, pytz
 
 def get_livegames():
+    """
+    requests JSON from NBA API, parses it and returns list of games
+    """
     url = "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json"
     response = requests.get(url)
     response_json = response.json()
@@ -34,6 +37,9 @@ def get_team_records(games):
     return records
 
 def get_livescores(games):
+    """
+    returns list of pairwise string scores that are the away [0] and home [1]
+    """
     scores = []
     for game in games:
         away_score = str(game["awayTeam"]["score"])
@@ -45,6 +51,9 @@ def get_livescores(games):
     return scores
 
 def get_gamestatus(games):
+    """
+    returns list of strings representing the status of game
+    """
     status = []
     for game in games:
         status_text = game["gameStatusText"].strip()
@@ -53,10 +62,37 @@ def get_gamestatus(games):
         elif (status_text == "Half"):
             status.append("HALF")
         else: # when status is the game clock or starting date
-            status.append(status_text)
+            if (len(status_text.split()) == 3): # convert ET to MT
+                pass
+            else:
+                status.append(status_text)
     return status
 
+def convert_PT_to_MT(pt_str):
+    """
+    converts PT time to MT time
+    """
+    pt_tz = pytz.timezone("US/Eastern")
+    mt_tz = pytz.timezone("US/Mountain")
+    
+    pt_str = pt_str.split()[0] + " " + pt_str.split()[1]
+    dt_pt = datetime.strptime(pt_str, "%I:%M %p")
+    dt_pt = pt_tz.localize(dt_pt)
+    
+    # need to provide Y/M/D due to time offsets happening, stack overflow has a lot of cases where pytz "bugs out"
+    dt_curr = datetime.now()
+    dt_pt = datetime(dt_curr.year, dt_curr.month, dt_curr.day, dt_pt.hour, dt_pt.minute)
+    dt_pt = pt_tz.localize(dt_pt)
+    
+    dt_mt = dt_pt.astimezone(mt_tz)
+    mt_str = dt_mt.strftime("%I:%M") + " pm MT"
+    mt_str = mt_str.strip("0")
+    return mt_str
+
 def get_gamestatus_colour(game_status, game_count):
+    """
+    returns list of hex color strings dependent on the status of game
+    """
     gamestatus_colour = []
     for i in range(game_count):
         status_text = game_status[i]
@@ -68,8 +104,10 @@ def get_gamestatus_colour(game_status, game_count):
             gamestatus_colour.append("#1e162f") # live game
     return gamestatus_colour
 
-        
 def assign_logos(matchups):
+    """
+    returns list of pairwise string paths to logos corresponding their respective matchup, away [0] home [1]
+    """
     logos = []
     for matchup in matchups:
         away_logo = "..\\static\\images\\logos\\" + matchup[0] + ".svg"
