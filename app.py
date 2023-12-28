4d2d373390a13f84578524befbf8b7075d5e5802
Hanss <hanss.rivera@gmail.com>
2023-12-27 18:30:50 -0700

    checkpoint anim

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from static.scoreboard import *
+from static.home_functions import *
 import requests
 
 app = Flask(__name__)
--- static/home_scripts.js
+++ static/home_scripts.js
@@ -12,23 +12,49 @@ function set_homevalues(data){
         set_logos(i, logos)
         set_name_rec(i, matchups, team_records)
         set_status_and_color(i, game_status, gamestatus_colour)
+        
     }
 }
 
 function set_scores(i, livescores){
+    animate_scores(i, livescores)
     $("#score_a_" + i).text(livescores[i][0])
     $("#score_h_" + i).text(livescores[i][1])
 }
+function animate_scores(i, livescores){
+    var $score_a = $("#score_a_" + i)
+    var $score_h = $("#score_h_" + i)
+    // console.log($score_a.text())
+    // console.log(livescores[i][0])
+
+    if ($score_a.text() != livescores[i][0]){
+        $score_a.addClass("changed_score")
+        setTimeout(function(){
+            $score_a.removeClass("changed_score")
+        }, 2600)
+    }
+    if ($score_h.text() != livescores[i][1]){
+        
+        $score_h.addClass("changed_score")
+        setTimeout(function(){
+            $score_h.removeClass("changed_score")
+        }, 2600)
+        
+    }
+}
+
 function set_logos(i, logos){
     $("#logo_a_" + i).attr("src", logos[i][0])
     $("#logo_h_" + i).attr("src", logos[i][1])
 }
+
 function set_name_rec(i, matchups, team_records){
     $("#abv_a_" + i).text(matchups[i][0])
     $("#abv_h_" + i).text(matchups[i][1])
     $("#rec_a_" + i).text(team_records[i][0])
     $("#rec_h_" + i).text(team_records[i][1])
 }
+
 function set_status_and_color(i, game_status, gamestatus_colour){
     $("#match_status_" + i).text(game_status[i])
     $("#match_container_" + i).css("background-color", gamestatus_colour[i]);
