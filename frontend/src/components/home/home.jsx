8178b4df59bf1dc43c266839e269f3c277bfde19
Hanss <hanss.rivera@gmail.com>
2024-01-07 21:51:21 -0700

    checkpoint

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,36 +1,16 @@
-import Header1 from "./header1.jsx"
 import Navbar from "../global/navbar.jsx"
+import Header1 from "./header1.jsx"
+import Match from "./match.jsx"
+
 
 function Home(){
     return (
         <>
         <Navbar/>
         <Header1/>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quisquam temporibus corporis aspernatur non dolores ut vero blanditiis amet, consectetur neque voluptates distinctio. Autem odio dolorem quibusdam ad et. Sit?</p>
+        <Match/>
+        <Match/>
+        <Match/>
         </>
     )
 }
---
+++ frontend/src/components/home/match.jsx
@@ -0,0 +1,21 @@
+import "./match.css"
+
+function Match(){
+    return(
+        <>
+        <div className="match_wrapper">
+
+
+        <a href="" className="match_link">
+        <div className="match_container">
+            
+        </div>
+        </a>
+
+
+        
+        </div>
+        </>
+    )
+}
+export default Match
