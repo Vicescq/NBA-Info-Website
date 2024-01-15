b9ff0929270aee10edd31fda9b7bfc57f08be78f
Hanss <hanss.rivera@gmail.com>
2024-01-14 22:21:11 -0700

    eye toggle css full functionality

--- frontend/src/components/navbar/eye.jsx
+++ frontend/src/components/navbar/eye.jsx
@@ -31,88 +31,105 @@ function EyeNavDropDown(props){
 }
 
 function EyeNavDropDownItem(props){
-    const [pressed, setPressed] = useState(false)
     const matchups = props.matchups
     const index = props.index
     const game_count = props.game_count
-    const item_onclick = (if_all, pressed) => {
+    const item_onclick = (if_all) => {
         if (if_all){
-            const score_arr = []
-            for (let i = 0; i < game_count; i++){
-                const score1 = document.getElementById("match_" + i).children[0].children[0]
-                const score2 = document.getElementById("match_" + i).children[2].children[1]
-                score_arr.push([score1, score2])
-            }
-
-            let i = 0
-            const visiblity_tracker = []
-            while(i < game_count){
-                if (score_arr[i][0].style.visibility == "hidden"){
-                    visiblity_tracker.push(0)
-                }
-                else{
-                    visiblity_tracker.push(1)
-                }
-                i += 1
-            }
-
-            const set = new Set(visiblity_tracker)
-            if (set.size == 1){
-                for (let i = 0; i < game_count; i++){
-
-                    
-                    if(visiblity_tracker[0] == 1){
-                        score_arr[i][0].style.visibility = "hidden"
-                        score_arr[i][1].style.visibility = "hidden"
-                    }
-                    else{
-                        score_arr[i][0].style.visibility = "visible"
-                        score_arr[i][1].style.visibility = "visible"
-                    }
-                }
-            }
-            else{
-                for (let i = 0; i < game_count; i++){
-                    if (visiblity_tracker[i]){
-                        score_arr[i][0].style.visibility = "hidden"
-                        score_arr[i][1].style.visibility = "hidden"
-                    }
-                }
-            }
-            
+            const score_arr = gather_score_DOMS(game_count)
+            const visibility_tracker = probe_score_visiblity_states(game_count, score_arr)
+            toggling_logic_scores(game_count, score_arr, visibility_tracker)
         }
         else{
+            const eyenavitem = document.getElementById("eyenavdropdownitem_" + index)
             const score1 = document.getElementById("match_" + index).children[0].children[0]
             const score2 = document.getElementById("match_" + index).children[2].children[1]
-            if (pressed == false){
-                
+            console.log(score1.style.visibility)
+            if (score1.style.visibility == "visible" || score1.style.visibility == ""){   // "" bc it starts as "" on first load
                 score1.style.visibility = "hidden"
-                score2.style.visibility = "hidden"   
+                score2.style.visibility = "hidden"
+                eyenavitem.style.textDecoration = "line-through"
+                eyenavitem.style.backgroundColor = "#b3152a"
+
             }
             else{
-                
                 score1.style.visibility = "visible"
                 score2.style.visibility = "visible"
+                eyenavitem.style.textDecoration = ""
+                eyenavitem.style.backgroundColor = "#242222"
             }
-            setPressed(!pressed)
-            
         }
     }
 
-    
-    
-
-
     return(
         <>
-        {index == 0 ? <li className="eyenavdropdownitem eyeall" onClick={() => {item_onclick(1, pressed)}}>ALL</li>: null}
-        <li className="eyenavdropdownitem" onClick={() => {item_onclick(0, pressed)}}>
+        {index == 0 ? <li className="eyenavdropdownitem eyeall" onClick={() => {item_onclick(1)}}>ALL</li>: null}
+        <li id={"eyenavdropdownitem_" + index} className="eyenavdropdownitem" onClick={() => {item_onclick(0)}}>
             {matchups[index][0]} vs {matchups[index][1]}
         </li>
         </>
     )
 }
 
+function gather_score_DOMS(game_count){
+    const score_arr = []
+    for (let i = 0; i < game_count; i++){
+        const score1 = document.getElementById("match_" + i).children[0].children[0]
+        const score2 = document.getElementById("match_" + i).children[2].children[1]
+        score_arr.push([score1, score2])
+    }
+    return score_arr   
+}
+
+function probe_score_visiblity_states(game_count, score_arr){
+    let i = 0
+    const visibility_tracker = []
+    while(i < game_count){
+        if (score_arr[i][0].style.visibility == "hidden"){
+            visibility_tracker.push(0)
+        }
+        else{
+            visibility_tracker.push(1)
+        }
+        i += 1
+    }
+    return visibility_tracker
+}
+
+function toggling_logic_scores(game_count, score_arr, visibility_tracker){
+    const set = new Set(visibility_tracker)
+    if (set.size == 1){
+        if(visibility_tracker[0] == 1){
+            for (let i = 0; i < game_count; i++){
+                const eyenavitem = document.getElementById("eyenavdropdownitem_" + i)
+                score_arr[i][0].style.visibility = "hidden"
+                score_arr[i][1].style.visibility = "hidden"
+                eyenavitem.style.textDecoration = "line-through"
+                eyenavitem.style.backgroundColor = "#b3152a"
+            }
+        }
+        else{
+            for (let i = 0; i < game_count; i++){
+                const eyenavitem = document.getElementById("eyenavdropdownitem_" + i)
+                score_arr[i][0].style.visibility = "visible"
+                score_arr[i][1].style.visibility = "visible"
+                eyenavitem.style.textDecoration = ""
+                eyenavitem.style.backgroundColor = "#242222"
+            }
+        }
+    }
+    else{
+        for (let i = 0; i < game_count; i++){
+            const eyenavitem = document.getElementById("eyenavdropdownitem_" + i)
+            if (visibility_tracker[i]){
+                score_arr[i][0].style.visibility = "hidden"
+                score_arr[i][1].style.visibility = "hidden"
+                eyenavitem.style.textDecoration = "line-through"
+                eyenavitem.style.backgroundColor = "#b3152a"
+            }
+        }
+    }
+}
 
 export default EyeNavItem
 
